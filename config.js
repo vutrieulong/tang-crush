65d8aae7cdd8ec08e5f9ce55bb42e15b0c856859
vutrieulong <85732390+vutrieulong@users.noreply.github.com>
2021-06-11 15:22:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,12 @@ const CONFIG = {
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnNo: 'Không, Anh iu à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <3 iu anh waaa, tối bé thưởng anh nhaaaa',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay videocall với anh nhé công chúa.',
+    messDesc: 'Zậy tối nay mình videocall nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com/longvu04' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
